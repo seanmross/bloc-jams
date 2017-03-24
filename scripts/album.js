dbfc0948e30b574c6f68a741ee017805f58450b1
Sean Ross <seanmross9@gmail.com>
2017-03-24 18:50:52 -0400

    Assignment 13: completed

--- scripts/album.js
+++ scripts/album.js
@@ -63,13 +63,21 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-  if (element) {
-      var currentParent = element.parentElement;
-      while (currentParent.className !== targetClass && currentParent.className !== null) {
-          currentParent = currentParent.parentElement;
+    if (element) {
+        var currentParent = element.parentElement;
+        if (currentParent === null){
+          console.log('No parent found');
+        } else {
+          while (currentParent.className !== targetClass && currentParent.className !== null) {
+              currentParent = currentParent.parentElement;
+          }
+          if (currentParent.className === null){
+            console.log('No parent found with that class name');
+          } else {
+            return currentParent;
+          }
         }
-        return currentParent;
-      }
+    }
 };
 
 var getSongItem = function(element) {
